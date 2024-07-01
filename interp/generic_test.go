474b15cd75584cad4a9fcb1e12f912b7770f1f76
Kai O'Reilly <koreilly5297@gmail.com>
2024-07-01 19:16:12 -0400

    add TestGenericFuncComplex to demonstrate issue

--- interp/generic_test.go
+++ interp/generic_test.go
@@ -5,6 +5,21 @@ import (
 	"testing"
 )
 
+func TestGenericFuncDeclare(t *testing.T) {
+	i := New(Options{})
+	_, err := i.Eval("func Hello[T comparable](v T) *T {\n\treturn &v\n}")
+	if err != nil {
+		t.Error(err)
+	}
+	res, err := i.Eval("Hello(3)")
+	if err != nil {
+		t.Error(err)
+	}
+	if res.Elem().Interface() != 3 {
+		t.Error("expected &(3), got", res)
+	}
+}
+
 func TestGenericFunc(t *testing.T) {
 	i := New(Options{})
 	err := i.Use(Exports{
@@ -25,13 +40,18 @@ func TestGenericFunc(t *testing.T) {
 	}
 }
 
-func TestDeclareGenericFunc(t *testing.T) {
+func TestGenericFuncComplex(t *testing.T) {
 	i := New(Options{})
-	_, err := i.Eval("func Hello[T comparable](v T) *T {\n\treturn &v\n}")
+	err := i.Use(Exports{
+		"guthib.com/generic/generic": map[string]reflect.Value{
+			"Hello": reflect.ValueOf(GenericFunc("func Hello[T comparable, F any](v T, f func(*T) *F) *T {\n\treturn &v\n}")),
+		},
+	})
 	if err != nil {
 		t.Error(err)
 	}
-	res, err := i.Eval("Hello(3)")
+	i.ImportUsed()
+	res, err := i.Eval("generic.Hello(3)")
 	if err != nil {
 		t.Error(err)
 	}
