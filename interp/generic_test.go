ecc6fde02667cbeae17e6ae7717cfcf173fe7587
Kai O'Reilly <koreilly5297@gmail.com>
2024-07-02 09:13:03 -0400

    reproduce generics issue: referencing a symbol in the same package

--- interp/generic_test.go
+++ interp/generic_test.go
@@ -44,14 +44,15 @@ func TestGenericFuncComplex(t *testing.T) {
 	i := New(Options{})
 	err := i.Use(Exports{
 		"guthib.com/generic/generic": map[string]reflect.Value{
-			"Hello": reflect.ValueOf(GenericFunc("func Hello[T comparable, F any](v T, f func(a T) F) *T {\n\treturn &v\n}")),
+			"Do":    reflect.ValueOf(func() {}),
+			"Hello": reflect.ValueOf(GenericFunc("func Hello[T comparable, F any](v T, f func(a T) F) *T {\n\tDo(); return &v\n}")),
 		},
 	})
 	if err != nil {
 		t.Error(err)
 	}
 	i.ImportUsed()
-	res, err := i.Eval("generic.Hello(3, func(a int) bool { return true })")
+	res, err := i.Eval("generic.Hello[int, bool](3, func(a int) bool { return true })")
 	if err != nil {
 		t.Error(err)
 	}
