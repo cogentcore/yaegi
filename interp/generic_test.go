5506698277e0b83251bc7d7c28367d092f868737
Randall C. O'Reilly <oreilly@ucdavis.edu>
2024-07-05 14:49:31 -0700

    generic test fixes: use . import to access methods in same scope as hoisted generic function package.

--- interp/generic_test.go
+++ interp/generic_test.go
@@ -42,16 +42,17 @@ func TestGenericFunc(t *testing.T) {
 
 func TestGenericFuncComplex(t *testing.T) {
 	i := New(Options{})
+	done := false
 	err := i.Use(Exports{
 		"guthib.com/generic/generic": map[string]reflect.Value{
-			"Do":    reflect.ValueOf(func() {}),
-			"Hello": reflect.ValueOf(GenericFunc("func Hello[T comparable, F any](v T, f func(a T) F) *T {\n\tDo(); return &v\n}")),
+			"Do":    reflect.ValueOf(func() { done = true }),
+			"Hello": reflect.ValueOf(GenericFunc("import . \"guthib.com/generic\"\nfunc Hello[T comparable, F any](v T, f func(a T) F) *T {\n\tDo(); return &v\n}")),
 		},
 	})
+	i.ImportUsed()
 	if err != nil {
 		t.Error(err)
 	}
-	i.ImportUsed()
 	res, err := i.Eval("generic.Hello[int, bool](3, func(a int) bool { return true })")
 	if err != nil {
 		t.Error(err)
@@ -59,21 +60,27 @@ func TestGenericFuncComplex(t *testing.T) {
 	if res.Elem().Interface() != 3 {
 		t.Error("expected &(3), got", res)
 	}
+	if !done {
+		t.Error("!done")
+	}
 }
 
 func TestCallPackageFunc(t *testing.T) {
 	i := New(Options{})
+	done := false
 	err := i.Use(Exports{
 		"guthib.com/generic/generic": map[string]reflect.Value{
-			"Do": reflect.ValueOf(func() {}),
+			"Do": reflect.ValueOf(func() { done = true }),
 		},
 	})
 	if err != nil {
 		t.Error(err)
 	}
-	i.ImportUsed()
-	_, err = i.Eval("package generic\nfunc Hello() { Do() }")
+	_, err = i.Eval("import . \"guthib.com/generic\"\nfunc Hello() { Do() }\nfunc main() { Hello() }")
 	if err != nil {
 		t.Error(err)
+	}
+	if !done {
+		t.Error("!done")
 	}
 }
